cd1238c7cc756c25f98f791210e34f516f5f6775
Deramer <arseniy.nestyuk@gmail.com>
2018-01-20 17:36:54 +0300

    Refactored code a little.
    I may hate Clojure style guides, but they still are style guides.

--- src/clojured_monster/game.clj
+++ src/clojured_monster/game.clj
@@ -6,13 +6,19 @@
 
 (defn create-game [ids]
     (let [game-state-key (apply min ids)
-          four-random-numbers (shuffle [(rand-int 10) (- (rand-int 20) 10) (- (rand-int 20) 10) (- (rand-int 20) 10) ])
-          monster {:mood (rand-int 100) :hit (nth four-random-numbers 0) :pat (nth four-random-numbers 1) :feed (nth four-random-numbers 2) :criticize (nth four-random-numbers 3)  }
+          four-random-numbers (shuffle [(rand-int 10) 
+                                        (- (rand-int 20) 10) 
+                                        (- (rand-int 20) 10) 
+                                        (- (rand-int 20) 10)])
+          monster {:mood (rand-int 100) 
+                   :hit (nth four-random-numbers 0) 
+                   :pat (nth four-random-numbers 1) 
+                   :feed (nth four-random-numbers 2) 
+                   :scold (nth four-random-numbers 3)  }
           new-ids (reduce into {} (map #(hash-map %  false) ids))
           ]
-      (swap! game-states #(merge % {game-state-key {:monster-chars monster :ids new-ids :game-over false} })))
-      (texts :start-of-the-game)
-)
+      (swap! game-states #(merge % {game-state-key {:monster-chars monster :ids new-ids :game-over false}})))
+      (texts :start-of-the-game))
   
 (defn compute-changes [id action game-state-key]
    (let [game-state (@game-states game-state-key)
@@ -25,23 +31,15 @@
             (let [
                   game-over? (if (= action :tame) (if (> mood 100) true false) false)
                   id-game-over? (if (= action :tame) (if (<= mood 100) true false) false)
-                  new-mood (if (= action :hit) (+ mood  (:hit monster)) new-mood)
-                  new-mood (if (= action :pat) (+ mood  (:pat monster)) new-mood)
-                  new-mood (if (= action :feed) (+ mood  (:feed monster)) new-mood)
-                  new-mood (if (= action :criticize) (+ mood  (:criticize monster)) new-mood)
-                  message (str "user " id " " action " the monster, "
-                              (if  game-over? " the monster becomes his/her friend"
-                                  (if id-game-over? "the user was eaten" 
-                                    (if (> (- new-mood mood) 0) " the monster is pleased" " the monster get angry" )
-                                  )
-                              )
-                            )
+                  new-mood (+ mood (action monster) new-mood)
+                  message (str "user " id " " 
+                               action " the monster, "
+                               (if game-over? 
+                                 " the monster becomes his/her friend"
+                                 (if id-game-over? 
+                                   "the user was eaten" 
+                                   (if (> (- new-mood mood) 0) " the monster is pleased" " the monster get angry"))))
                   new-monster (merge monster {:mood new-mood})
-                  new-ids (merge (game-state :ids) {id id-game-over?})
-            ]
+                  new-ids (merge (game-state :ids) {id id-game-over?})]
         (swap! game-states #(merge % {game-state-key {:monster-chars new-monster :ids new-ids :game-over  game-over?}}))
-        message
-        )
-      )
-  )
-)
+        message))))
