764c1be4f80e86a288843f03cf7126b779af6cdf
Julia-Shaykhutdinova <35668321+Julia-Shaykhutdinova@users.noreply.github.com>
2018-01-22 02:52:11 +0300

    moved action descriptions and endings here

--- resources/english_text.clj
+++ resources/english_text.clj
@@ -8,4 +8,11 @@
  :error "Message text is not a command. Misprint or just some trash."
  :start-of-the-game "Welcome, students, to your first Care of Magical Creatures class. In this cage, behind a curtain, is a monster for you. Your number one task is to not get eaten; to earn an A, please tame this little nice monster. Will it be a gentle niffler or a dangerous hippogriff, you ask? You'll get to know this first, I promise. You only have one try to tame the monster, because a monster does not just decline your offer -- it eats you instead. Find your own way of taming the monster first to win. Put on your gloves and take some treats. Don't forget: not everyone likes to be petted."
  :game-over-user-trying-action "Здесь могла бы быть ваша реклама, но это ваш некролог. Hogwarts will miss you"
+ :tame "tried to tame"
+ :hit "hit"
+ :pet "strokes"
+ :scold "scolds"
+ :feed "gives a treat to"
+ :good-ending "the monster becomes your new friend!"
+ :bad-ending "the monster swallows you whole"
  }
