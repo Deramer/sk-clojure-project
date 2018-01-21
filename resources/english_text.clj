1c39da6f5a36bd358a0aeb3d37e1b05a2384388f
Julia-Shaykhutdinova <35668321+Julia-Shaykhutdinova@users.noreply.github.com>
2018-01-22 01:53:51 +0300

    Update english_text.clj

--- resources/english_text.clj
+++ resources/english_text.clj
@@ -1,11 +1,11 @@
 {
- :start "First interaction with bot, never happens again."
- :help "Help request."
+ :start "Welcome, students, to your first Care of Magical Creatures class. In this cell, behind a curtain, is a monster for you. Your number one task is to not get eaten; to earn an A, please tame this little nice monster. Will it be a gentle niffler or a dangerous hippogriff, you ask? You'll get to know this first, I promise. You only have one try to tame the monster, because a monster does not just decline your offer -- it eats you instead. Find your own way of taming the monster first to win. Put on your gloves and take some treats. Don't forget: not everyone likes to be petted. feed -- give your treats to the monster. pet -- caress the monster's neck. scold -- \"Who's the bad boy?\". hit -- flick the monster's nose and show who is the boss. tame -- try to tame the monster. You can only try once!"
+ :help "feed -- give your treats to the monster. pet -- caress the monster's neck. scold -- \"Who's the bad boy?\". hit -- flick the monster's nose and show who is the boss. tame -- try to tame the monster. You can only try once!"
  :stand-by "Player asked for a game and now should wait for other players."
  :joined-game "Player joined a game."
  :error-in-game "Player IS in a game but acts like he is NOT. Politely tell him to stop."
  :error-out-of-game "Player is NOT in a game but acts like he IS. Politely tell him to stop."
  :error "Message text is not a command. Misprint or just some trash."
- :start-of-the-game "text for the start-of-the-game"
+ :start-of-the-game "Welcome, students, to your first Care of Magical Creatures class. In this cell, behind a curtain, is a monster for you. Your number one task is to not get eaten; to earn an A, please tame this little nice monster. Will it be a gentle niffler or a dangerous hippogriff, you ask? You'll get to know this first, I promise. You only have one try to tame the monster, because a monster does not just decline your offer -- it eats you instead. Find your own way of taming the monster first to win. Put on your gloves and take some treats. Don't forget: not everyone likes to be petted."
  :game-over-user-trying-action "you are dead"
  }
