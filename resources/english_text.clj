b80b00972e7b8fe07cc873fe4a7226d9fe76460d
Julia-Shaykhutdinova <35668321+Julia-Shaykhutdinova@users.noreply.github.com>
2018-01-22 02:26:56 +0300

    Update english_text.clj

--- resources/english_text.clj
+++ resources/english_text.clj
@@ -4,7 +4,7 @@
  :stand-by "You are in front of the cage. Please wait for other students."
  :joined-game "A new student joined."
  :error-in-game "Why are you waiting? The monster is ready. Go on!"
- :error-out-of-game "The class hasn't started yet. Type play to start."
+ :error-out-of-game "The class hasn't started yet. Type play to start or wait for groupmates to join."
  :error "Message text is not a command. Misprint or just some trash."
  :start-of-the-game "Welcome, students, to your first Care of Magical Creatures class. In this cage, behind a curtain, is a monster for you. Your number one task is to not get eaten; to earn an A, please tame this little nice monster. Will it be a gentle niffler or a dangerous hippogriff, you ask? You'll get to know this first, I promise. You only have one try to tame the monster, because a monster does not just decline your offer -- it eats you instead. Find your own way of taming the monster first to win. Put on your gloves and take some treats. Don't forget: not everyone likes to be petted."
  :game-over-user-trying-action "Здесь могла бы быть ваша реклама, но это ваш некролог. Hogwarts will miss you"
