2c8d16e78eb4144a0f1b1b96f5c8076e4bb048e7
Julia-Shaykhutdinova <35668321+Julia-Shaykhutdinova@users.noreply.github.com>
2018-01-22 02:06:20 +0300

    Update english_text.clj

--- resources/english_text.clj
+++ resources/english_text.clj
@@ -1,5 +1,5 @@
 {
- :start "Welcome, students, to your first Care of Magical Creatures class. In this cell, behind a curtain, is a monster for you. Your number one task is to not get eaten; to earn an A, please tame this little nice monster. Will it be a gentle niffler or a dangerous hippogriff, you ask? You'll get to know this first, I promise. You only have one try to tame the monster, because a monster does not just decline your offer -- it eats you instead. Find your own way of taming the monster first to win. Put on your gloves and take some treats. Don't forget: not everyone likes to be petted. feed -- give your treats to the monster. pet -- caress the monster's neck. scold -- \"Who's the bad boy?\". hit -- flick the monster's nose and show who is the boss. tame -- try to tame the monster. You can only try once!"
+ :start "Welcome to the Game. Type /play to start or /help for a list of commands."
  :help "feed -- give your treats to the monster. pet -- caress the monster's neck. scold -- \"Who's the bad boy?\". hit -- flick the monster's nose and show who is the boss. tame -- try to tame the monster. You can only try once!"
  :stand-by "Player asked for a game and now should wait for other players."
  :joined-game "Player joined a game."
